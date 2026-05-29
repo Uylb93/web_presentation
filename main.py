9d12583755df7f4afb9c640c66ff3b8254d99ecf
Uriel <Uriel.LozaBahena@hotmail.com>
2026-05-29 18:33:27 -0500

    finish presentation

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
